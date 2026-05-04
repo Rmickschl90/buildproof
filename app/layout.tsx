e6048908af4603074b85a1f7b416c23466819cdb
Ryan Mickschl <rmickschl23@gmail.com>
2026-05-04 01:19:33 -0500

    Remove temporary mobile build version indicator

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,20 +30,6 @@ export default function RootLayout({
         <OfflineSendIndicator />
         {children}
         <OfflineSendBootstrap />
-
-        {/* TEMP VERSION CHECK */}
-        <div
-          style={{
-            position: "fixed",
-            bottom: 4,
-            right: 6,
-            fontSize: 10,
-            opacity: 0.5,
-            zIndex: 9999,
-          }}
-        >
-          v1-mobile-test
-        </div>
       </body>
     </html>
   );
